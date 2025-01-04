253efe54eec018827a59b8115ee8e0933ea05afe
ryan-ott <51638926+ryan-ott@users.noreply.github.com>
2025-01-04 12:11:28 +0000

    Deploying to gh-pages from @ ryan-ott/ryan-ott.github.io@2fd28dfc109078fea23ce6e8689800d314d51a36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
